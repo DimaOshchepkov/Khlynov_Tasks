247868ab45763c13025367317de13ccfa246349f
DmitiyOshchepkov <odo.2003@yandex.ru>
2023-04-26 20:56:43 +0300

    4 point of the technical task

--- Задание 1/main.py
+++ Задание 1/main.py
@@ -4,6 +4,7 @@ from get_curs import *
 from ResponceBD import *
 import logging
 import re
+import sqlite3
 
 if __name__ == '__main__':
 
@@ -36,6 +37,29 @@ if __name__ == '__main__':
     SenderResponce.apply(name_db, [Response(curency_order_response), Response(currency_rates_response)])
 
     add_in_db(name_db, vcodes, date)
+
+   # Подключение к базе данных
+    conn = sqlite3.connect(name_db)
+    c = conn.cursor()
+
+    # Формирование SQL-запроса с использованием параметров
+    query = '''SELECT CURRENCY_RATES.*, CURRENCY_ORDER.*
+            FROM CURRENCY_RATES
+            JOIN CURRENCY_ORDER ON CURRENCY_RATES.order_id = CURRENCY_ORDER.id
+            WHERE CURRENCY_RATES.numeric_code IN ({})'''.format(','.join(['?']*len(vcodes)))
+    
+    # Выполнение SQL-запроса и извлечение результатов
+    c.execute(query, tuple(vcodes))
+    rows = c.fetchall()
+
+    # Вывод результатов
+    for row in rows:
+        print(row)
+
+    # Закрытие соединения с базой данных
+    conn.close()
+
+    
 
 
 
