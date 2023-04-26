fcd0badd4e3b670d0b240fa526f8c922d1f61859
DmitiyOshchepkov <odo.2003@yandex.ru>
2023-04-26 18:25:46 +0300

    add comments

--- Задание 1/main.py
+++ Задание 1/main.py
@@ -7,32 +7,34 @@ from dataclasses import dataclass
 
 @dataclass
 class Response:
-    __insert_responce : str = None
+    """ DB request class. Stores a string and a parameter"""    
+    __responce : str = None
     __data : list[tuple] = None
 
-    def __init__(self, insert_responce: str, data : list[tuple] = []) -> None:
-        self.__insert_responce = insert_responce
+    def __init__(self, responce: str, data : list[tuple] = []) -> None:
+        self.__responce = responce
         self.__data = data
     
     @property
-    def insert_responce(self) -> str:
-        return self.__insert_responce
+    def responce(self) -> str:
+        return self.__responce
     
     @property
     def data(self) -> list[tuple]:
         return self.__data
     
 class SenderResponce:
+    """Static class that sends Responses to BD"""
 
     @staticmethod
-    def apply(name_bd: str, inserts_responce : list[Response]) -> None:
+    def apply(name_bd: str, responces : list[Response]) -> None:
         
         conn = sqlite3.connect(name_bd)
         cursor = conn.cursor()
 
-        for ins_res in inserts_responce:
+        for res in responces:
             # Execute SQL query to insert data into CURRENCY_RATES table
-            cursor.execute(ins_res.insert_responce, ins_res.data)
+            cursor.execute(res.responce, res.data)
             conn.commit()
 
         # Commit the transaction and close connection
@@ -40,18 +42,45 @@ class SenderResponce:
         conn.close()
 
 
-def GetCursOnDateXML(date: str) -> None:
+def get_curs_on_date_XML(date: str) -> etree._Element:
+    """
+    Request for http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?wsdl
 
+    The data is structured like this:
+    <ValuteData>
+        <ValuteCursOnDate>
+            <Vname>Японская иена                                                                                                                                                                                                                                                 </Vname>
+            <Vnom>100</Vnom>
+            <Vcurs>60.6164</Vcurs>
+            <Vcode>392</Vcode>
+            <VchCode>JPY</VchCode>
+        </ValuteCursOnDate>
+    </ValuteData>
+
+    Args:
+        date (str): date for which you need to get information about the currency
+
+    Returns:
+        etree._Element: xml tree
+    """
     client = Client('http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?wsdl')
     # Construct the SOAP request payload
     request_payload = {
         'On_date': date  # Replace with the actual value for the 'On_date' parameter
     }
 
-    return client.service.GetCursOnDateXML(**request_payload)
+    return client.service.get_curs_on_date_XML(**request_payload)
 
-def find_info_currency_from_xml(xml : etree, code : str) -> tuple:
+def find_info_currency_from_xml(xml : etree._Element, code : str) -> tuple:
+    """_summary_
 
+    Args:
+        xml (etree._Element): xml tree, which is obtained by the get_curs_on_date_XML request 
+        code (str): currency code
+
+    Returns:
+        tuple: (vch_code.text, vname.text, vnom.text, vcurs.text, vch_code)
+    """
     vcode = xml.xpath(f".//Vcode[normalize-space(text())={code}][1]")[0]
     valute_curs_on_date = vcode.getparent()
     vname = valute_curs_on_date.find("Vname")
@@ -64,9 +93,17 @@ def find_info_currency_from_xml(xml : etree, code : str) -> tuple:
 
 
 def add_in_db(name_db : str, Vcodes : list, date : str) -> None:
+    """
+    Adds information about currencies by Vcodes to the database if this information is not contained there
+
+    Args:
+        name_db (str): name of the database
+        Vcodes (list): Currency codes
+        date (str):  date for which you need to get information about the currency
+    """    
 
     try:
-        xml = GetCursOnDateXML(date)
+        xml = get_curs_on_date_XML(date)
     except Exception as e:
         print("Ошибка при получении данных c сервиса\n", e)
         print(e)
